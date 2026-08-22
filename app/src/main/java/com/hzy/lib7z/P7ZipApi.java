3b593ebaf8df87e3e776c5f33f27abd37cc04248
sahisahil9393-rgb <sahisahil9393@gmail.com>
2026-08-23 00:57:29 +0100

    Create P7ZipApi.java

---
+++ app/src/main/java/com/hzy/lib7z/P7ZipApi.java
@@ -0,0 +1,18 @@
+package com.hzy.libp7zip;
+
+/**
+ * Local stub for the missing com.hzy.libp7zip.P7ZipApi
+ * from the unavailable AndroidP7zip library.
+ * 
+ * NOTE: 7z extraction via command line will NOT work at runtime.
+ * This stub only allows the project to compile.
+ */
+public class P7ZipApi {
+    
+    /**
+     * Executes a 7z command. Stub returns -1 (error).
+     */
+    public static int executeCommand(String command) {
+        return -1;
+    }
+}
